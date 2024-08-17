c40376d10c61d3d3837c261e487c2aa84bbfd206
Jerry <jerryshum2636@gmail.com>
2024-08-17 17:10:44 -0400

    stlying courses

--- front_end/src/features/courses/CourseItem.jsx
+++ front_end/src/features/courses/CourseItem.jsx
@@ -4,10 +4,11 @@ const CourseItem = ({ title, icon, link }) => {
   return (
     <Link
       to={`/courses${link}`}
-      className="flex w-1/4 flex-col items-center justify-center rounded-md bg-blue-800 p-4 text-white duration-200 hover:bg-blue-600"
+      className="flex w-1/4 flex-col items-center justify-center rounded-md border-2 border-sky-500 bg-blue-900 p-4 text-white duration-200 hover:bg-blue-800"
+      style={{ textDecoration: 'none' }}
     >
-      <h2 className="mb-2 text-xl font-bold">{title}</h2>
-      <div className="image-container">
+      <h2 className="mb-6 text-xl font-bold">{title}</h2>
+      <div className="image-container rounded-md border border-black">
         <img src={icon} alt="Course Icon" className="rounded-md" />
       </div>
     </Link>
--- front_end/src/ui/Footer.jsx
+++ front_end/src/ui/Footer.jsx
@@ -1,8 +1,8 @@
 function Footer() {
   return (
-    <div className="bg-gray-900 py-4 text-white">
+    <div className="bg-blue-500 py-4 text-white">
       <div className="container mx-auto text-center">
-        <p className="text-sm">This is the footer</p>
+        <p className="text-sm"></p>
       </div>
     </div>
   );
