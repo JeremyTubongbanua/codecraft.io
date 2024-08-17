a58c60604ddc069371f624ec8002dc0ffb2c9c86
Jerry <jerryshum2636@gmail.com>
2024-08-17 16:49:32 -0400

    styled login screen

--- front_end/src/features/login/Login.jsx
+++ front_end/src/features/login/Login.jsx
@@ -22,29 +22,49 @@ function Login() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="flex h-full items-center justify-center bg-gray-100">
-        <div className="w-80 rounded-lg bg-white p-8 shadow-lg">
-          <input
-            type="text"
-            placeholder="Username"
-            className="mb-4 w-full rounded border border-gray-300 p-2 text-black"
-            value={username}
-            onChange={handleUsername}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            className="mb-6 w-full rounded border border-gray-300 p-2 text-black"
-            value={password}
-            onChange={handlePassword}
-          />
-          <button className="w-full rounded bg-blue-300 py-2 text-white duration-200 hover:bg-blue-400">
-            Login
-          </button>
+    <div className="flex h-screen flex-col">
+      <header className="bg-blue-600 py-4">
+        <h1 className="text-center text-3xl font-bold text-white">
+          codecraft.io
+        </h1>
+      </header>
+      <form onSubmit={handleSubmit} className="h-full">
+        <div className="flex h-full items-center justify-center bg-gray-100">
+          <div className="w-80 rounded-lg bg-white p-8 shadow-lg">
+            <h2 className="mb-6 text-2xl font-bold text-blue-600">Login</h2>
+            <div className="mb-4">
+              <label htmlFor="username" className="text-gray-700">
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                placeholder="Enter your username"
+                className="w-full rounded border border-gray-300 p-2 text-black"
+                value={username}
+                onChange={handleUsername}
+              />
+            </div>
+            <div className="mb-6">
+              <label htmlFor="password" className="text-gray-700">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Enter your password"
+                className="w-full rounded border border-gray-300 p-2 text-black"
+                value={password}
+                onChange={handlePassword}
+              />
+            </div>
+            <button className="w-full rounded bg-blue-500 py-2 text-white duration-200 hover:bg-blue-600">
+              Login
+            </button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 }
 
