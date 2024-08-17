fbd9e6070de3944ee3e131d44fb60407f45cf8f4
Jerry <jerryshum2636@gmail.com>
2024-08-16 23:27:43 -0400

    defining file routing system

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,7 +1,21 @@
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AppLayout from './ui/AppLayout';
+import Home from './ui/Home';
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <BrowserRouter></BrowserRouter>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
