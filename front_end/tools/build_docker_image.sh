f11e4fc8ef8d678b7560c09ed5fa742d0e2837a1
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2024-08-17 13:16:02 -0400

    feat: tools use script_directory bash

--- front_end/tools/build_docker_image.sh
+++ front_end/tools/build_docker_image.sh
@@ -3,6 +3,9 @@ FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
 cd "$SCRIPT_DIRECTORY"
 
+git fetch
+git reset --hard upstream/main
+
 cd ..
 npm i
 npm run build
