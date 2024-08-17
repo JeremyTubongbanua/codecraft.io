e22ba8d9b5002cde05d59f97e59168597d739333
Jeremy Tubongbanua <jeremy.tubongbanua@gmail.com>
2024-08-16 23:37:03 -0400

    feat: login

--- front_end/src/features/login/Login.jsx
+++ front_end/src/features/login/Login.jsx
@@ -1,5 +1,43 @@
+import React, { useState } from 'react';
+
 function Login() {
-  return <div>login</div>;
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(username == 'demo' && password == 'demo') {
+            alert('Login successful!');
+            // @jerry add code to redirect to courses
+        }
+    };
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Username:
+                    <input type="text" value={username} onChange={handleUsernameChange} className="text-black" />
+                </label>
+                <br />
+                <label>
+                    Password:
+                    <input type="password" value={password} onChange={handlePasswordChange} className="text-black"  />
+                </label>
+                <br />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
 }
 
 export default Login;
