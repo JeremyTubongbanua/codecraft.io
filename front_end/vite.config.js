70df90b785a03853292711680262a6cb7a9bd0fb
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2024-08-17 22:00:12 +0000

    feat: update vite.confnig.js

--- front_end/vite.config.js
+++ front_end/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['@uiw/react-textarea-code-editor'],
+      output: {
+        globals: {
+          '@uiw/react-textarea-code-editor': 'ReactTextareaCodeEditor'
+        }
+      }
+    }
+  }
+});
