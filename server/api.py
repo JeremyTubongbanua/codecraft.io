a5878099976067c76b2a9d25a0cb481dbbd1059b
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2024-08-17 04:15:33 +0000

    feat: working docker file

--- server/api.py
+++ server/api.py
@@ -4,7 +4,7 @@ from flask import request
 app = Flask(__name__)
 
 @app.route('/c', methods=['POST'])
-def handle_request():
+def handle_c_request():
     data = request.get_json()
     # we're going to receive a json body like this
     # {
@@ -34,6 +34,36 @@ def handle_request():
         "data": result.stdout.decode('utf-8')
     }
 
+
+@app.route('/python', methods=['POST'])
+def handle_python_request():
+    data = request.get_json()
+
+    with open('temp.py', 'w') as f:
+        for line in data['data']:
+            f.write(line + '\n')
+
+    import subprocess
+    result = subprocess.run(['python3', 'temp.py'], stdout=subprocess.PIPE)
+
+    return {
+        "data": result.stdout.decode('utf-8')
+    }
+
+@app.route('/javascript', methods=['POST'])
+def handle_javascript_request():
+    data = request.get_json()
+
+    with open('temp.js', 'w') as f:
+        for line in data['data']:
+            f.write(line + '\n')
+
+    import subprocess
+    result = subprocess.run(['node', 'temp.js'], stdout=subprocess.PIPE)
+
+    return {
+        "data": result.stdout.decode('utf-8')
+    }
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
---
+++ server/examples/example.py
@@ -0,0 +1,8 @@
+def add(x, y):
+    return x + y
+
+def main():
+    print(str(add(1, 2)))
+    print("Hello, World!")
+
+main()
