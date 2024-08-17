8734a47d8cf022c459979bb9306911f9539b98e7
Jerry <jerryshum2636@gmail.com>
2024-08-17 17:00:33 -0400

    feat: styled the header

--- front_end/src/ui/Header.jsx
+++ front_end/src/ui/Header.jsx
@@ -4,21 +4,30 @@ const Header = () => {
   return (
     <div className="text-center font-sans">
       {/* Title Section */}
-      <div className="border-b-2 border-blue-400 bg-blue-300 p-5">
-        <Link to="/home" className="m-0 text-5xl text-white">
-          CodeCraft
+      <div className="bg-blue-600 p-5 py-4">
+        <Link to="/home" className="text-center text-3xl font-bold text-white">
+          codecraft.io
         </Link>
       </div>
 
       {/* Navigation Section */}
-      <div className="flex justify-around bg-blue-200 p-3">
-        <Link to="/courses" className="text-xl text-black no-underline">
+      <div className="flex justify-between bg-blue-800">
+        <Link
+          to="/courses"
+          className="w-1/3 border-r-2 border-white px-4 py-2 text-xl font-semibold text-white no-underline duration-200 hover:bg-blue-900"
+        >
           Courses
         </Link>
-        <Link to="/about" className="text-xl text-black no-underline">
+        <Link
+          to="/about"
+          className="w-1/3 border-r-2 border-white px-4 py-2 text-xl font-semibold text-white no-underline duration-200 hover:bg-blue-900"
+        >
           About
         </Link>
-        <Link to="/" className="text-xl text-black no-underline">
+        <Link
+          to="/"
+          className="w-1/3 px-4 py-2 text-xl font-semibold text-white no-underline duration-200 hover:bg-blue-900"
+        >
           Logout
         </Link>
       </div>
